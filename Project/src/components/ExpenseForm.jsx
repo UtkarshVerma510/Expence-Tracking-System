a349d8082fef849947189658bb2a8b043785346f
Utkarsh Verma <utkarshv4100@gmail.com>
2026-06-03 07:23:59 +0530

    Version 1

---
+++ Project/src/components/ExpenseForm.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+export default function ExpenseForm({addExpense}) {
+  const [input, setInput] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const handleChangeAmount = (e) => {
+    setAmount(e.target.value);
+    
+  };
+  const handleChangeName = (e) => {
+    setInput(e.target.value);
+
+  };
+
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    const newExpense = {
+        id: Date.now(),
+        date: new Date().toDateString(),
+        name: input,
+        amount: amount,
+    };
+    addExpense(newExpense);
+    // console.log(input,"and the amount is ", amount)
+    setAmount("");
+    setInput("");
+}
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Expense name:</label>
+      <input type="text"  value = {input} required onChange={handleChangeName} />
+      <label>Expanse amount:</label>
+      <input
+        type="number"
+     
+        value={amount}
+        required
+        onChange={handleChangeAmount}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
